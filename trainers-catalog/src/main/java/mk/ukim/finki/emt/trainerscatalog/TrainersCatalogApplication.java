83873c0dd3550ba0a8111e69118e9e2a93d8d7d5
makedonkavasilevaa <99400411+makedonkavasilevaa@users.noreply.github.com>
2024-05-22 14:32:56 +0200

    DDD first phase

---
+++ trainers-catalog/src/main/java/mk/ukim/finki/emt/trainerscatalog/TrainersCatalogApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.trainerscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainersCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainersCatalogApplication.class, args);
+    }
+
+}
---
+++ trainers-catalog/src/test/java/mk/ukim/finki/emt/trainerscatalog/TrainersCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.trainerscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainersCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
